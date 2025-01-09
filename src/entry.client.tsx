e42757ee4e985dded72710e87f7148a739029be4
kashafdawood <kashaf.dawood@gmail.com>
2025-01-09 08:04:37 +0500

    setup apollo client

--- src/entry.client.tsx
+++ src/entry.client.tsx
@@ -1,5 +1,12 @@
 import ReactDOM from "react-dom/client";
 import { HydratedRouter } from "react-router/dom";
+import { ApolloProvider } from "@apollo/client";
+import client from "./apolloClient";
 import "./index.css";
 
-ReactDOM.hydrateRoot(document, <HydratedRouter />);
+ReactDOM.hydrateRoot(
+  document,
+  <ApolloProvider client={client}>
+    <HydratedRouter />
+  </ApolloProvider>
+);
