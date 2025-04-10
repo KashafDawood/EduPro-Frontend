8cabe20c3d6de27121bdad60997b7d1703eba828
kashafdawood <kashaf.dawood@gmail.com>
2025-04-10 21:34:06 +0500

    Refactor code structure and remove redundant changes

--- src/Components/MRTable/schema/schema-helpers.ts
+++
@@ -1,2 +0,0 @@
-export const defaultStyles = { maxHeight: "70vh", width: "90vw" };
-export const defaultSize = 200;
--- src/Components/MRTable/types.ts
+++
@@ -1,6 +0,0 @@
-import { MRT_ColumnDef } from "material-react-table";
-
-export interface TableSchema {
-  TableSchema: MRT_ColumnDef<JSON, unknown>[];
-  styles: { [key: string]: string };
-}
